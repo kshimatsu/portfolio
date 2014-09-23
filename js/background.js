722d8725292cec28ceeb1b44c41013512904e957
kshimatsu <kristenshimatsu@gmail.com>
2014-09-23 17:03:35 +0800

    Changed button to just avoid default of #

--- js/background.js
+++ js/background.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
 		return '#' + Math.random().toString(16).slice(2,8);
 	}
 
-	$('#bgBtn').on("click", function() {
+	$('#bgBtn').on("click", function(event) {
+		event.preventDefault();
 		console.log("Hello World!");
 
 		$('.navbar').css('background', randomColor());
 	});
+
+
 });
